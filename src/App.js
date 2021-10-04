18b2ddc5ca4cad514978e40cb0bdf979e5e97a21
Bree <breehorton25@gmail.com>
2021-10-03 20:00:32 -0400

    firebase working - pprinting to page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import { useState, useEffect } from 'react';
 import realtime from './firebase.js'
-import { ref, onValue } from 'firebase/database';
+import { ref, onValue, push } from 'firebase/database';
 import './App.css';
+import UserInput from './UserInput.js';
 
 
 function App() {
-  // put in own component
-  const [userDate, setUserDate] = useState('');
-  const [userTime, setUserTime] = useState('');
-  const [userAccomp, setUserAccomp] = useState(''); 
+
+const [userDate, setUserDate] = useState('');
+const [userTime, setUserTime] = useState('');
+const [userAccomp, setUserAccomp] = useState(''); 
+const [userJournal, setUserJournal] = useState([]);
 
   useEffect(() => {
     const dbRef = ref(realtime);
+    
+
 
     onValue(dbRef, (snapshot) => {
-      console.log(snapshot.val());
+      const myData = snapshot.val();
+      const entryArray = [];
+
+      for (let propertyName in myData) {
+        const journalObject= {
+          key: propertyName,
+          input: myData[propertyName]
+        }
+        entryArray.push(journalObject);
+      }
+      // userJournal(entryArray);
+      setUserJournal(entryArray);
     });
   }, []);
+
+  const handleDateChange = (e) => {
+    setUserDate(e.target.value);
+  }
+  const handleTimeChange = (e) => {
+    setUserTime(e.target.value);
+  }
+  const handleTextChange = (e) => {
+    setUserAccomp(e.target.value);
+  } 
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    
+
+    
+    const dbRef = ref(realtime);
+
+    push(dbRef, userDate);
+    push(dbRef, userTime);
+    push(dbRef, userAccomp);
+  }
+
+  
+  
+
+  
+
 
   return (
     <div className="App">
@@ -27,19 +72,58 @@ function App() {
       </header>
       <main>
         <div className="wrapper">
-          <form action="">
+          <form 
+          onSubmit={handleSubmit}
+          >
             <div className="dateContainer">
-              <label htmlFor="date">Date: </label>
-              <input type="date" />
+                <label htmlFor="date">Date: </label>
+                <input 
+                type="date"
+                id='date'
+                onChange={handleDateChange}
+                value={userDate}
+                />
             </div>
             <div className="timeContainer">
-              <label htmlFor="time">Time: </label>
-              <input type="time" />
+                <label htmlFor="time">Time: </label>
+                <input 
+                type="time"
+                id='time'
+                onChange={handleTimeChange}
+                value={userTime}
+                />
             </div>
 
             <label className='sr-only' htmlFor="accomplishment">What I accomplished today: </label>
-            <textarea name="accomplishment" id="accomplishment" cols="30" rows="10" placeholder='What I accomplished today...'></textarea>
-          </form>
+            <textarea 
+            onChange={handleTextChange}
+            value={userAccomp}
+            name="accomplishment" 
+            id="accomplishment" cols="30" rows="10" placeholder='What I accomplished today...'></textarea>
+            <button>Log Today's Practice</button>
+        </form>
+        <div className="inputContainer">
+          <p>Date: {userDate}</p>
+          <p>Time: {userTime}</p>
+          <p>What I accomplished today: {userAccomp} </p>
+        </div>
+          
+        
+          {/* {
+            userJournal.map((infoEntered) => {
+                return (
+                  <div key={} className="inputContainer">
+                    <p>Date: {infoEntered.date}</p>
+                    <p>Time: {infoEntered.time}</p>
+                    <p>What I accomplished today: {infoEntered}</p>
+                  </div>
+                  <UserInput 
+                  key={infoEntered.key}
+                  date={infoEntered.userDate}
+                  />
+                  );
+                })
+              } */}
         </div>
       </main>
       <footer>
---
+++ src/UserInput.js
@@ -0,0 +1,11 @@
+
+// function UserInput(props) {
+//     return(    
+//         <div className="inputContainer">
+//             <p>Date: {props.date}</p>
+//             <p>Time: </p>
+//             <p>What I accomplished today: </p>
+//         </div>
+//     )
+// }
+// export default UserInput;
